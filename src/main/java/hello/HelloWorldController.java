e4113cfa93f66fe10b422c4c6b93fa9da0abe798
Anderson Silveira <anderson.silveira.br@gmail.com>
2017-03-19 23:35:49 -0300

    google goecoder

--- src/main/java/hello/HelloWorldController.java
+++ src/main/java/hello/HelloWorldController.java
@@ -1,6 +1,7 @@
 package hello;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,7 @@ import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.google.maps.GeoApiContext;
 import com.google.maps.GeocodingApi;
 import com.google.maps.model.ComponentFilter;
@@ -26,22 +28,26 @@ public class HelloWorldController {
 
         System.out.println(jsonData);
         String action = getAction(jsonData);
-        
-        GeoApiContext context = new GeoApiContext().setApiKey("AIzaSyCIN10kIcLKR4SVCaI21bq0_KrfPbfFgJI");
-        
-        GeocodingResult[] results = null;
-	try {
-	    results = GeocodingApi.newRequest(context)
-		    .address("R. dos aimorés, 335, Campinas - SP")
-		    .components(ComponentFilter.country("BRA")).await();
-	    System.out.println("lat"+ results[0].geometry.location.lat);
-	    System.out.println("long"+ results[0].geometry.location.lng);
-	} catch (Exception e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
-	}
-        	
-        return new WebhookResponse("Olá! Confira no mapa de esse é o seu endereço ->\n http://maps.google.com/?q="+results[0].geometry.location.lat+","+results[0].geometry.location.lng, "Text " + action);
+        if(action.equalsIgnoreCase("getAddress")){
+            String adrress = getAdrress(jsonData);
+            GeoApiContext context = new GeoApiContext().setApiKey("AIzaSyCIN10kIcLKR4SVCaI21bq0_KrfPbfFgJI");
+            
+            GeocodingResult[] results = null;
+            try {
+        	results = GeocodingApi.newRequest(context)
+        		.address(adrress)
+        		.components(ComponentFilter.country("BRA")).await();
+        	System.out.println("lat"+ results[0].geometry.location.lat);
+        	System.out.println("long"+ results[0].geometry.location.lng);
+            } catch (Exception e) {
+        	// TODO Auto-generated catch block
+        	e.printStackTrace();
+            }
+            return new WebhookResponse("Olá! Confira no mapa de esse é o seu endereço\n link "
+            	+ "-> http://maps.google.com/?q="+results[0].geometry.location.lat+","+results[0].geometry.location.lng, "Text " + action);
+            
+        }
+        return new WebhookResponse("Test","Test");
     }
 
     private String getAction(String jsonData) {
@@ -62,4 +68,35 @@ public class HelloWorldController {
 	}
 	return "";
     }
+    
+    private String getAdrress(String jsonData) {
+   	ObjectMapper objectMapper = new ObjectMapper();
+           try {
+               JsonNode rootNode = objectMapper.readTree(jsonData.getBytes());
+             
+               JsonNode result = rootNode.path("result");
+               ArrayNode context = (ArrayNode) result.path("contexts");
+               Iterator<JsonNode> fields = context.elements();
+               while(fields.hasNext()){
+        	   JsonNode next = fields.next();
+        	   JsonNode nameTag = next.path("name");
+		if(nameTag.asText().equals("citynamecontext")){
+		    JsonNode parameters = next.path("parameters");
+		    String streetType = parameters.path("streetType.original").asText();
+		    String strName = parameters.path("any.original").asText();
+		    String addressNumber = parameters.path("addressNumber.original").asText();
+		    String addressCity = parameters.path("addressCity.original").asText();
+		    return streetType+" "+strName+" "+addressNumber+" "+addressCity;
+        	  }
+               }
+               return null;
+   	} catch (JsonParseException e) {
+   	    e.printStackTrace();
+   	} catch (JsonMappingException e) {
+   	    e.printStackTrace();
+   	} catch (IOException e) {
+   	    // TODO Auto-generated catch block
+   	}
+   	return "";
+       }
 }
